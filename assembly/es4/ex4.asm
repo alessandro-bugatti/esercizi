9dbe6b93936c8612787b37692fb52bf238a4be24
alessandro-bugatti <alessandro.bugatti@gmail.com>
2017-10-19 15:05:43 +0200

    FIx error

--- assembly/es4/ex4.asm
+++ assembly/es4/ex4.asm
@@ -11,9 +11,9 @@
         LOADiH R2 0x03 
         LOADiL R2 0xE6 
         LOADiH R3 0x00 
-        LOADiL R3 0x01 
+        LOADiL R3 0x66 
 CARICA: STORE R1 R3
-        ADD R3 R3 0x2
+        ADD R3 R3 0x3
         ADD R1 R1 0x2
         BLE R1 R2 CARICA
 
@@ -24,10 +24,13 @@ CARICA: STORE R1 R3
         LOADiH R2 0x03
         LOADiL R3 0x00
         LOADiH R3 0x04
+        LOADiL R5 0x01
+        LOADiH R5 0x00
 CICLO:  LOAD R4 R1 
-        SUB R4 R4 0x01
+        SUB R4 R4 R5
         STORE R3 R4
         ADD R1 R1 0x2
         ADD R3 R3 0x2
+        ADD R5 R5 0x1
         BLE R1 R2 CICLO
 FINE:   END
