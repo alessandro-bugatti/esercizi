8c716cca13ff1172f3736767f5bd2fe9c799fd3e
alessandro-bugatti <alessandro.bugatti@gmail.com>
2017-10-31 12:22:24 +0100

    Improve example

--- programmazione_C/funzioni/scambio/main.c
+++ programmazione_C/funzioni/scambio/main.c
@@ -18,6 +18,11 @@ void scambio_vero(int *a, int *b)
 int main()
 {
     int a = 7, b = 5;
+    printf("Valori iniziali, a = %d, b = %d\n", a, b);
+    //Qua non avviene nulla, passaggio per valore
+    scambio(a, b);
+    printf("a = %d, b = %d\n", a, b);
+    //Qua avviene lo scambio, passaggio per indirizzo
     scambio_vero(&a, &b);
     printf("a = %d, b = %d\n", a, b);
     return 0;
