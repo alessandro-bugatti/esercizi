9c6bccc369b125efdcc1518950aa3887312a18ae
alessandro-bugatti <alessandro.bugatti@gmail.com>
2017-11-16 15:55:45 +0100

    Add new example

---
+++ programmazione_C/stringhe/funzioni_standard/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//int main()
+//{
+//    char s1[10], s2[] = "Parola",
+//    s3[] = "Frase troppo lunga per stare in s1";
+//    strncpy(s1, s2, 9);
+//    printf("s1: %s\ns2: %s\n", s1, s2);
+//    strncpy(s1, s3, 9);
+//    printf("s1: %s\ns3: %s\n", s1, s3);
+//    return 0;
+//}
+
+//int main()
+//{
+//    char s1[] = "amico", s2[] = "amicizia",
+//    s3[] = "Amore";
+//    if (strncmp(s1, s2, 10) > 0)
+//        printf("%s viene dopo %s in ordine alfabetico\n", s1, s2);
+//    if (strncmp(s3, s1, 10) < 0)
+//        printf("%s viene prima di %s in ordine alfabetico\n", s3, s1);
+//    if (strncmp(s1, s2, 4) == 0)
+//        printf("I primi quattro caratteri di %s e %s sono uguali\n",
+//                s1, s2);
+//    return 0;
+//}
+
+//int main()
+//{
+//    char s1[30], s2[] = "a tutti";
+//    strncpy(s1,"Ciao ", 29);
+//    printf("s1: %s \n", s1, s2);
+//    strncat(s1, s2, 29);
+//    printf("s1: %s \n", s1, s2);
+//    return 0;
+//}
---
+++ programmazione_C/stringhe/funzioni_standard/readme.md
@@ -0,0 +1,7 @@
+# Funzioni standard di libreria
+
+Provare il funzionamento delle funzioni standard di libreria delle stringhe:
+
+* strncpy: per la copia di stringhe
+* strncmp: per il confronto tra stringhe
+* strncat: per la concatenazione tra stringhe
