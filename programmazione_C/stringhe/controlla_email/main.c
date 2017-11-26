4ad997dced2f647973d8411e3374f363c328c6b0
alessandro-bugatti <alessandro.bugatti@gmail.com>
2017-11-26 23:45:53 +0100

    Add condition

--- programmazione_C/stringhe/controlla_email/main.c
+++ programmazione_C/stringhe/controlla_email/main.c
@@ -18,11 +18,42 @@ int controlla_prima_parte(char s[])
     return 0;
 }
 
+int solo_alfabetici(char s[])
+{
+    int i;
+    for (i = 0; s[i] != '\0'; i++)
+        if (!isalpha(s[i]))
+            return 0;
+    return 1;
+}
+
+int controlla_seconda_parte(char s[])
+{
+    int i, pos_chiocciola, quanti_punti = 0;
+    //Trova la posizione della chiocciola
+    for (i = 0; s[i] != '@'; i++);
+    pos_chiocciola = i;
+    for (; s[i] != '\0'; i++)
+        if (s[i] == '.')
+            quanti_punti++;
+    if (quanti_punti != 1)
+        return 0;
+    if (s[pos_chiocciola + 1] == '.')
+        return 0;
+    if (s[i-3] != '.' && s[i-4] != '.')
+        return 0;
+    if (s[i-4] == '.' && solo_alfabetici(&s[i-3]))
+        return 1;
+    if (s[i-3] == '.' && solo_alfabetici(&s[i-2]))
+        return 1;
+    return 0;
+}
+
 int controlla_mail(char elenco[][100], int n)
 {
     int i, conta = 0;
     for (i = 0; i < n; i++)
-        if(chiocciola(elenco[i]) == 1 && controlla_prima_parte(elenco[i]) == 1)
+        if(chiocciola(elenco[i]) == 1 && controlla_prima_parte(elenco[i]) == 1 && controlla_seconda_parte(elenco[i]))
             conta++;
     return conta;
 }
@@ -31,7 +62,7 @@ int controlla_mail(char elenco[][100], int n)
 
 int main()
 {
-    char c[][100] = {"ale@gmail.com","al@ggl.it","ioio@@io@i"};
+    char c[][100] = {"ale@gmail.com","al@gli.ut","ioio@@io@i"};
     printf("Indirizzi validi: %d\n", controlla_mail(c,3));
     return 0;
 }
