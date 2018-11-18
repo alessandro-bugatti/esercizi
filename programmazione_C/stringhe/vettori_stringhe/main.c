64d91f2c8d67933b2b46a1c965d73f7eb6b012ea
alessandro-bugatti <alessandro.bugatti@gmail.com>
2018-11-18 23:15:35 +0100

    Improve example

--- programmazione_C/stringhe/vettori_stringhe/main.c
+++ programmazione_C/stringhe/vettori_stringhe/main.c
@@ -7,8 +7,11 @@ int main()
     int i;
     printf("Inserisci una parola: ");
     fgets(stringhe[0], 30, stdin);
-    strncpy(stringhe[1], "altra parola",29);
-    strncpy(stringhe[2], stringhe[0],29);
+    strncpy(stringhe[1], "altra parola",30);
+    strncpy(stringhe[2], stringhe[0],30);
+    //Questa istruzione elimina il carattere di a capo
+    //presente in stringhe[2] a causa della fgets
+    stringhe[2][strlen(stringhe[2])- 1] = '\0';
     strncat(stringhe[2], stringhe[1],12);
     for (i = 0; i < 3; i++)
         printf("stringhe[%d]: %s\n", i, stringhe[i]);
