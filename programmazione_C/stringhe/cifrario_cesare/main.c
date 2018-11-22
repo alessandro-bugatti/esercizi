89d32cd3df1f796c4d5db3972e149f8bead19af4
alessandro-bugatti <alessandro.bugatti@gmail.com>
2018-11-22 22:51:24 +0100

    Improve adding decoding

--- programmazione_C/stringhe/cifrario_cesare/main.c
+++ programmazione_C/stringhe/cifrario_cesare/main.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void cifratura_cesare(char s[], char c[], int n)
+void cifratura_cesare(char chiaro[], char cifrato[], int n)
 {
+    n = n % 26;
     int i;
-    for (i = 0; s[i] != '\0'; i++)
-        c[i] = (s[i] - 'a' + n)%26 + 'a';
-    c[i] = '\0';
+    for (i = 0; chiaro[i] != '\0'; i++)
+        cifrato[i] = (chiaro[i] - 'a' + n)%26+ 'a';
+    cifrato[i] = '\0';
+}
+
+void decifratura_cesare(char cifrato[], char chiaro[], int n)
+{
+    n = n % 26;
+    int i;
+    for (i = 0; cifrato[i] != '\0'; i++)
+        chiaro[i] = (cifrato[i] - 'a' + 26 - n)%26 + 'a';
+    chiaro[i] = '\0';
 }
 
 int main()
 {
-    char s[]="attaccoallazonanemica";
-    char c[30];
-    cifratura_cesare(s,c,3);
-    printf("%s -> %s\n", s, c);
+    char chiaro[]="attaccoallazonanemica";
+    char cifrato[100];
+    char decifrato[100];
+    printf("Codifica\n");
+    cifratura_cesare(chiaro, cifrato, 3);
+    printf("Chiaro: %s\n", chiaro);
+    printf("Cifrato: %s\n", cifrato);
+
+    int i;
+    printf("\n--------------\n\nDecodifica a forza bruta provando tutte le chiavi\n");
+    for (i = 0; i < 26; i++){
+        decifratura_cesare(cifrato, decifrato, i);
+        printf("Chiave %d\n", i);
+        printf("Cifrato: %s\n", cifrato);
+        printf("Decifrato: %s\n", decifrato);
+    }
     return 0;
 }
