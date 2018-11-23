ca3034208fbeb2ddfd64f50682ab808be2035dd9
alessandro-bugatti <alessandro.bugatti@gmail.com>
2018-11-23 22:28:44 +0100

    Improve solution

--- programmazione_C/stringhe/cifrario_sostituzione/main.c
+++ programmazione_C/stringhe/cifrario_sostituzione/main.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void cifrario_sostituzione(char *s, char *c)
+void cifrario_sostituzione(char chiaro[], char cifrato[], char chiave[])
 {
     int i;
-    for (i = 0; s[i] != '\0'; i++)
-        s[i] = c[s[i] - 'a'];
+    for (i = 0; chiaro[i] != '\0'; i++)
+        cifrato[i] = chiave[chiaro[i] - 'a'];
+    cifrato[i] = '\0';
 }
 
+void decifratura_sostituzione(char cifrato[], char chiaro[],
+                              char chiave[])
+{
+    int i, j;
+    for (i = 0; cifrato[i] != '\0'; i++)
+    {
+        int posizione;
+        for (j = 0; j < 26; j++)
+            if (cifrato[i] == chiave[j])
+                posizione = j;
+        chiaro[i] = 'a' + posizione;
+    }
+    chiaro[i] = '\0';
+}
 int main()
 {
-    char c[] = "qwertyuiopasdfghjklzxcvbnm";
-    char s[] = "salutiatutti";
-    cifrario_sostituzione(s,c);
-    printf("%s\n", s);
+    char chiaro[] = "testoinchiaro";
+    char chiave[] = "qwertyuiopasdfghjklzxcvbnm";
+    char cifrato[100];
+    char decifrato[100];
+    int i, j;
+    cifrario_sostituzione(chiaro, cifrato, chiave);
+    printf("Chiaro %s\n", chiaro);
+    printf("Cifrato %s\n", cifrato);
+    decifratura_sostituzione(cifrato, decifrato, chiave);
+    printf("Cifrato %s\n", cifrato);
+    printf("Chiaro %s\n", decifrato);
     return 0;
 }
