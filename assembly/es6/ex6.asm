347b96f7ffb7bd7226ebd462657c2080d106326a
alessandro-bugatti <alessandro.bugatti@gmail.com>
2019-10-24 11:37:31 +0200

    Corregge l'esercizio

--- assembly/es6/ex6.asm
+++ assembly/es6/ex6.asm
@@ -4,10 +4,9 @@
 /aree di memoria 31 e 32 e scrive nella cella 33 il numero 1 se è 
 /compreso, 2 se invece è esterno all'intervallo. Si può supporre
 /che il numero nella cella 31 sia minore del numero nella cella 32
-
       LDR R1, 30
       LDR R2, 31
-      LDR R3, 45
+      LDR R3, 32
       MOV R4, #1
       MOV R5, #2
       CMP R1, R2
@@ -16,3 +15,7 @@
       BGT ESTERNO
       STR R4, 33
       B FINE
+ESTERNO:
+      STR R5, 33
+FINE:
+      HALT
