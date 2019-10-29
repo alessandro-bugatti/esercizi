2c38e3011cad4b00a1f3857882dd2192c008219e
alessandro-bugatti <alessandro.bugatti@gmail.com>
2019-10-29 12:49:28 +0100

    Aggiorna l'esercizio del lancio del proiettile

--- programmazione_C/iterazione/lancio_proiettile/main.c
+++ programmazione_C/iterazione/lancio_proiettile/main.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 
 #define G 9.81
+#define DELTA_T 0.01
 
 int main()
 {
     float vel_iniziale, theta;
     float x = 0, y = 0;
     float vx, vy;
-    float tempo = 0.1;
+    float tempo = 0;
+
     printf("Inserisci velocità iniziale e angolo in radianti");
     scanf("%f", &vel_iniziale);
     scanf("%f", &theta);
@@ -16,9 +18,9 @@ int main()
     vy = sin(theta)*vel_iniziale;
     do{
         printf("x: %f, y: %f\n",x,y);
+        tempo = tempo + DELTA_T;
         x = vx*tempo;
         y = vy*tempo - 0.5*G*tempo*tempo;
-        tempo = tempo + 0.1;
     }while (y > 0);
     return 0;
 }
