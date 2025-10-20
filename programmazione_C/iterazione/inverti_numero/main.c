d0cfce38dcc5e327b9e97c0cd7a2b45869f1cc51
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2025-10-20 14:55:47 +0200

    Corregge un errore

--- programmazione_C/iterazione/inverti_numero/main.c
+++ programmazione_C/iterazione/inverti_numero/main.c
@@ -3,15 +3,17 @@
 
 int main()
 {
-    int N, N_inverso = 0;
+    int N, N_inverso = 0, zero_in_testa = 1;
     printf("Inserisci N:");
     scanf("%d", &N);
     //Viene risolto in due modi, sia stampandoli al "volo"
     //sia creando una variabile che conterrà il risultato
     while (N>0)
     {
-        if (N%10 != 0)
+        if (N%10 != 0 || zero_in_testa == 0){
             printf("%d", N%10);
+            zero_in_testa = 0;
+		}
         N_inverso = N_inverso*10 + N%10;
         N = N/10;
     }
